38f3608677e734b9bfa2c87befdc01dfd88bfbc3
alex.karamushko <alex.karamushko@nordclan.com>
2021-03-03 22:52:35 +0300

    implement list traversal algorithm example

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,55 @@
-function walkAndUpdate(DOMElement: Element | null): void {
-  if (!DOMElement) {
-    return;
+/* eslint-disable no-constant-condition */
+/* eslint-disable no-continue */
+
+class Walker {
+
+  private root: Element | null = null;
+  private node: Element | null = null;
+  private walkBy = 0;
+
+  constructor(root: Element | null) {
+    this.root = root;
   }
 
-  const { children = [] } = DOMElement;
+  walkByAndDoWork(walkBy: number): void {
+    if (!this.root) {
+      return;
+    }
 
-  DOMElement.insertAdjacentHTML('beforebegin', '<span>Updating...</span>');
+    this.walkBy = 0;
 
-  Array.from(children).forEach(walkAndUpdate);
+    if (!this.node) {
+      this.node = this.root.firstElementChild;
+    }
+
+    while (true) {
+      if (this.walkBy === walkBy) {
+        return;
+      }
+
+      this.walkBy += 1;
+      this.node?.insertAdjacentHTML('beforebegin', '<span>Updating...</span>');
+
+      if (this.node?.children.length) {
+        this.node = this.node.firstElementChild;
+        continue;
+      }
+      if (this.node === this.root) {
+        return;
+      }
+      while (!this.node?.nextElementSibling) {
+        if (!this.node?.parentElement || this.node.parentElement === this.root) {
+          return;
+        }
+        this.node = this.node?.parentElement;
+      }
+      this.node = this.node?.nextElementSibling;
+    }
+  }
+
 }
 
-const trigger: HTMLInputElement | null = document.querySelector('.trigger');
+const walker = new Walker(document.querySelector('.list'));
 
-trigger?.addEventListener('input', () => {
-  walkAndUpdate(document.querySelector('.list'));
-});
+walker.walkByAndDoWork(2);
+walker.walkByAndDoWork(2);
